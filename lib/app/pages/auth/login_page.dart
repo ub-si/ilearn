1878c5c37dfa9871ce1c6d42540ae3ddd26f4641
Marcos David <marcos@appmake.com.br>
2023-05-15 21:24:46 -0300

    Pagina de Login

--- lib/app/pages/auth/login_page.dart
+++ lib/app/pages/auth/login_page.dart
@@ -1,15 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+import '../../core/config/app_colors.dart';
+import '../../controllers/auth_controller.dart';
+import '../../core/services/validators.dart';
+import '../../core/widgets/text_field_widget.dart';
+
+class SignInPage extends StatelessWidget {
+  SignInPage({Key? key}) : super(key: key);
+
+  final _formKey = GlobalKey<FormState>();
+  final emailTExtController = TextEditingController();
+  final passwordTExtController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              AppColors.primary,
+              AppColors.primaryDark,
+            ],
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: size.height,
+            width: size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset("assets/img/ilearn-logo-branca.png", scale: 1.5),
+                    ],
+                  ),
+                ),
+
+                // Formulário
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 40,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          // Email
+                          TextFieldWidget(
+                            controller: emailTExtController,
+                            icon: Icons.email,
+                            label: 'Email',
+                            validator: emailValidator,
+                          ),
+
+                          // Senha
+                          TextFieldWidget(
+                            controller: passwordTExtController,
+                            icon: Icons.lock,
+                            label: 'Senha',
+                            isSecret: true,
+                            validator: passwordValidator,
+                          ),
+
+                          // Botão de entrar
+                          SizedBox(
+                            height: 50,
+                            child: GetX<AuthController>(builder: (controller) {
+                              return ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: AppColors.primary,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18),
+                                  ),
+                                ),
+                                onPressed: controller.isLoading.value == true
+                                    ? null
+                                    : () {
+                                        FocusScope.of(context).unfocus();
+
+                                        if (_formKey.currentState!.validate()) {
+                                          String email = emailTExtController.text;
+                                          String password = passwordTExtController.text;
+                                          controller.signIn(email: email, password: password);
+                                        }
+                                      },
+                                child: controller.isLoading.value == true
+                                    ? const CircularProgressIndicator(backgroundColor: Colors.white)
+                                    : const Text(
+                                        'ENTRAR',
+                                        style: TextStyle(fontSize: 18),
+                                      ),
+                              );
+                            }),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
